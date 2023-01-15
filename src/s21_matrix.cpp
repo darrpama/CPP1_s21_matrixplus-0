92d756584c102effe514317c6c2e61a2882ea386
darrpama <darrpama@student.21-school.ru>
2023-01-15 05:46:11 +0300

    init project

---
+++ src/s21_matrix.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+
+class S21Matrix {
+	private:
+		// Attributes
+		int rows_, cols_;
+		double **matrix_;
+	public:
+		S21Matrix();
+		~S21Matrix();
+
+		void SumMatrix(const S21Matrix& other);
+		// Other methods...
+}
