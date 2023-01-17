ccbd6ca9aa5123d0296ab6b40265a397bc71777d
darrpama <darrpama@student.21-school.ru>
2023-01-17 22:40:55 +0300

    Now i use nvim as IDE instead VSCode

--- src/s21_matrix.cpp
+++ src/s21_matrix.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
 class S21Matrix {
-	private:
-		// Attributes
-		int rows_, cols_;
-		double **matrix_;
-	public:
-		S21Matrix();
-		~S21Matrix();
+  private:
+    // Attributes
+    int rows_, cols_;
+    double **matrix_;
+  public:
+    S21Matrix();
+    ~S21Matrix();
 
-		void SumMatrix(const S21Matrix& other);
-		// Other methods...
-}
+  void SumMatrix(const S21Matrix& other);
+  // Other methods...
+};
+
