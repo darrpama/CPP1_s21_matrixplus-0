9cf12c48d698117144d37d7f8ac8515f584f6f98
darrpama <darrpama@student.21-school.ru>
2023-01-22 21:50:32 +0300

    rewrite project structure, add .h

--- src/s21_matrix.cpp
+++ src/s21_matrix.cpp
@@ -1,53 +1,74 @@
-#include <iostream>
+#include "s21_matrix.h"
 
-class S21Matrix {
-private:
-  int rows_, cols_;
-  double **matrix_;
+S21Matrix::S21Matrix(int rows, int cols) {};
 
-public:
-  S21Matrix() {
+S21Matrix::S21Matrix(const S21Matrix& other) {};
 
-  };
-
-  S21Matrix(int rows, int cols) {
-
-  };
-
-  S21Matrix(const S21Matrix& other) {
-
-  };
-
-  S21Matrix(S21Matrix&& other) {
-
-  };
+S21Matrix::S21Matrix(S21Matrix&& other) {};
   
-  ~S21Matrix() {
+S21Matrix::~S21Matrix() {};
 
-  };
+bool S21Matrix::EqMatrix(const S21Matrix& other) {
+  return true;
+};
 
-  bool EqMatrix(const S21Matrix& other);
-  void SumMatrix(const S21Matrix& other);
-  void SubMatrix(const S21Matrix& other);
-  void MulNumber(const S21Matrix& other);
-  void MulMatrix(const S21Matrix& other);
+void S21Matrix::SumMatrix(const S21Matrix& other){};
+void S21Matrix::SubMatrix(const S21Matrix& other){};
+void S21Matrix::MulNumber(const S21Matrix& other){};
+void S21Matrix::MulMatrix(const S21Matrix& other){};
 
-  S21Matrix Transpose();
-  S21Matrix CalcComplements();
-  double Determinant();
-  S21Matrix InverseMatrix();
+S21Matrix S21Matrix::Transpose() {
+  return S21Matrix();
+};
 
-  S21Matrix operator + (const S21Matrix &other);
-  S21Matrix operator - (const S21Matrix &other);
-  S21Matrix operator * (const S21Matrix &other);
-  bool operator == (const S21Matrix &other);
-  bool operator != (const S21Matrix &other);
-  S21Matrix operator += (const S21Matrix &other);
-  S21Matrix operator -= (const S21Matrix &other);
-  S21Matrix operator *= (const S21Matrix &other);
-  double & operator () (int rows, int cols);
+S21Matrix S21Matrix::CalcComplements() {
+  return S21Matrix();
+};
+
+double S21Matrix::Determinant() {
+  return 1.0;
+};
+
+S21Matrix S21Matrix::InverseMatrix() {
+  return S21Matrix();
+};
+
+S21Matrix S21Matrix::operator + (const S21Matrix &other) {
+  return S21Matrix();
+};
+
+S21Matrix S21Matrix::operator - (const S21Matrix &other) {
+  return S21Matrix();
+};
+
+S21Matrix S21Matrix::operator * (const S21Matrix &other) {
+  return S21Matrix();
+};
+
+bool S21Matrix::operator == (const S21Matrix &other) {
+  return true;
+};
+
+bool S21Matrix::operator != (const S21Matrix &other) {
+  return true;
+};
+
+S21Matrix S21Matrix::operator += (const S21Matrix &other) {
+  return S21Matrix();
+};
+
+S21Matrix S21Matrix::operator -= (const S21Matrix &other) {
+  return S21Matrix();
+};
+
+S21Matrix S21Matrix::operator *= (const S21Matrix &other) {
+  return S21Matrix();
+};
+
+double& S21Matrix::operator () (int rows, int cols) {
 
 };
+
 
 int main(void) {
   return 0;
---
+++ src/s21_matrix.h
@@ -0,0 +1,40 @@
+#include <iostream>
+
+class S21Matrix {
+private:
+  int rows_, cols_;
+  double **matrix_;
+
+public:
+  S21Matrix();
+
+  S21Matrix(int rows, int cols);
+
+  S21Matrix(const S21Matrix& other);
+
+  S21Matrix(S21Matrix&& other);
+  
+  ~S21Matrix();
+
+  bool EqMatrix(const S21Matrix& other);
+  void SumMatrix(const S21Matrix& other);
+  void SubMatrix(const S21Matrix& other);
+  void MulNumber(const S21Matrix& other);
+  void MulMatrix(const S21Matrix& other);
+
+  S21Matrix Transpose();
+  S21Matrix CalcComplements();
+  double Determinant();
+  S21Matrix InverseMatrix();
+
+  S21Matrix operator + (const S21Matrix &other);
+  S21Matrix operator - (const S21Matrix &other);
+  S21Matrix operator * (const S21Matrix &other);
+  bool operator == (const S21Matrix &other);
+  bool operator != (const S21Matrix &other);
+  S21Matrix operator += (const S21Matrix &other);
+  S21Matrix operator -= (const S21Matrix &other);
+  S21Matrix operator *= (const S21Matrix &other);
+  double & operator () (int rows, int cols);
+
+};
