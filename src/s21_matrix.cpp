960373c1850fc3673f4db2689eed688dffb8ca89
darrpama <darrpama@student.21-school.ru>
2023-01-25 18:29:33 +0300

    push memory foo's into the top of class definition, add Moving method and EqMatrix

--- src/s21_matrix.cpp
+++ src/s21_matrix.cpp
@@ -1,20 +1,41 @@
 #include "s21_matrix.h"
+#include <stdexcept>
 
-S21Matrix::S21Matrix() {
+void S21Matrix::allocateMemory (const int rows, const int cols) {
+  this->matrix_ = new double *[rows];
+  for (int i = 0; i < cols; i++) {
+    this->matrix_[i] = new double[cols];
+  }
+};
+
+void S21Matrix::freeMemory() {
+  for (int i = 0; i < rows_; i++) {
+    delete [] matrix_[i];
+  }
+  delete matrix_;
+  rows_ = 0;
+  cols_ = 0;
+  matrix_ = nullptr;
+};
+
+S21Matrix::S21Matrix() {                          //  Constr
   this->rows_ = 2;
   this->cols_ = 2;
   this->matrix_ = new double *[rows_];
   allocateMemory(this->rows_, this->cols_);
 };
 
-S21Matrix::S21Matrix(int rows, int cols) {
+S21Matrix::S21Matrix(int rows, int cols) {        //  Constr
+  if (rows_ <= 0 || cols_ <= 0) {
+    throw std::invalid_argument("Incorrect size of matrix, each should be > 0");
+  }
   this->rows_ = rows;
   this->cols_ = cols;
   allocateMemory(this->rows_, this->cols_);
+
 };
 
-//  Copy
-S21Matrix::S21Matrix(const S21Matrix& other) {
+S21Matrix::S21Matrix(const S21Matrix& other) {    //  Copy
   this->cols_ = other.cols_;
   this->rows_ = other.rows_;
   for (int i = 0; i < other.rows_; i++) {
@@ -24,10 +45,19 @@ S21Matrix::S21Matrix(const S21Matrix& other) {
   }
 };
 
-//  Moving
-S21Matrix::S21Matrix(S21Matrix&& other) {
+S21Matrix::S21Matrix(S21Matrix&& other) {         //  Moving
+  this->cols_ = other.cols_;
+  this->rows_ = other.rows_;
+  for (int i = 0; i < other.rows_; i++) {
+    for (int j = 0; j < other.cols_; j++) {
+      this->matrix_[i][j] = other.matrix_[i][j];
+    }
+  }
+  other.matrix_ = nullptr;
+  other.cols_ = 0;
+  other.rows_ = 0;
 };
-  
+
 S21Matrix::~S21Matrix() {
   if (matrix_ != nullptr) {
     freeMemory();
@@ -35,13 +65,23 @@ S21Matrix::~S21Matrix() {
 };
 
 bool S21Matrix::EqMatrix(const S21Matrix& other) {
-  return true;
+  bool answer = false;
+  if (this->rows_ == other.rows_ && this->cols_ == other.cols_) {
+    for (int i = 0; i < rows_; i++) {
+      for (int j = 0; j < cols_; j++) {
+        if (fabs(matrix_[i][j]) - other.matrix_[i][j] < 0.0000001) {
+
+        }
+      }
+    }
+  }
+  return answer;
 };
 
-void S21Matrix::SumMatrix(const S21Matrix& other){};
-void S21Matrix::SubMatrix(const S21Matrix& other){};
-void S21Matrix::MulNumber(const S21Matrix& other){};
-void S21Matrix::MulMatrix(const S21Matrix& other){};
+void S21Matrix::SumMatrix(const S21Matrix& other) {};
+void S21Matrix::SubMatrix(const S21Matrix& other) {};
+void S21Matrix::MulNumber(const S21Matrix& other) {};
+void S21Matrix::MulMatrix(const S21Matrix& other) {};
 
 S21Matrix S21Matrix::Transpose() {
   return S21Matrix();
@@ -95,22 +135,34 @@ double& S21Matrix::operator () (int row, int col) {
   return this->matrix_[row][col];
 };
 
-void S21Matrix::allocateMemory(const int rows, const int cols) {
-  this->matrix_ = new double *[rows];
-  for (int i = 0; i < cols; i++) {
-    this->matrix_[i] = new double[cols];
-  }
-};
+int S21Matrix::GetRows() {
+  return this->rows_;
+}
 
-void S21Matrix::freeMemory() {
-  for (int i = 0; i < rows_; i++) {
-    delete [] matrix_[i];
+int S21Matrix::GetCols() {
+  return this->cols_;
+}
+
+// void S21Matrix::SetRows(int rows) {
+//   if (rows <= 0) {
+//     throw std::invalid_argument("Incorrect size of rows, it should be > 0");
+//   }
+//   S21Matrix newMatrix(*this);
+// }
+//
+// void S21Matrix::SetCols(int rows) {
+//   if (rows <= 0) {
+//     throw std::invalid_argument("Incorrect size of cols, it should be > 0");
+//   }
+// }
+
+void S21Matrix::FillMatrix(double value) {
+  for (int i = 0; i < this->rows_; i++) {
+    for (int j = 0; j < this->cols_; j++) {
+      this->matrix_[i][j] = value;
+    }
   }
-  delete matrix_;
-  rows_ = 0;
-  cols_ = 0;
-  matrix_ = nullptr;
-};
+}
 
 int main(void) {
   return 0;
